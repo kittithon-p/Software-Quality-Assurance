f7ae2fb64191639fae12b78ddec1e3543a268044
kittithon <33775691+kkaarrdd@users.noreply.github.com>
2019-10-12 18:04:56 +0700

    top down update

--- Integration-Testing/src/Top_down_integration/List_Stubs.java
+++ Integration-Testing/src/Top_down_integration/List_Stubs.java
@@ -1,5 +1,152 @@
 package Top_down_integration;
 
 public class List_Stubs {
-
+	
+	//CalculateSocialInsuranceFee_14000
+		public static double StubCalculateSocialInsuranceFee_14000(double salary) {
+			return 750;	
+		}
+		public static double StubCalculateOT_14000(double salary, double hour){
+			return 1000;
+		}
+		public static double StubCalculateTax_14000(double salary, double OT) {
+			return 450;
+		}
+		public static double CalculateNetSalary_14000(double salary, double extrahour)
+		{	double netsalary = 0.0;
+			double socialinsuranc = StubCalculateSocialInsuranceFee_14000(salary);
+			double OT= StubCalculateOT_14000(salary, extrahour);
+			double tax = StubCalculateTax_14000(salary, OT);
+							
+			netsalary = (salary+OT)-tax-socialinsuranc;
+			
+			System.out.println("Net Salary: " + netsalary);
+		
+			return netsalary;
+		}	
+	//CalculateSocialInsuranceFee_17000
+		public static double StubCalculateSocialInsuranceFee_17000(double salary) {
+			return 800;	
+		}
+		public static double StubCalculateOT_17000(double salary, double hour){
+			return 1250;
+		}
+		public static double StubCalculateTax_17000(double salary, double OT) {
+			return 912.5;
+		}
+		public static double CalculateNetSalary_17000(double salary, double extrahour)
+		{	double netsalary = 0.0;
+			double socialinsuranc = StubCalculateSocialInsuranceFee_17000(salary);
+			double OT= StubCalculateOT_17000(salary, extrahour);
+			double tax = StubCalculateTax_17000(salary, OT);
+							
+			netsalary = (salary+OT)-tax-socialinsuranc;
+			
+			System.out.println("Net Salary: " + netsalary);
+		
+			return netsalary;
+		}			
+	//CalculateSocialInsuranceFee_22000
+		public static double StubCalculateSocialInsuranceFee_22000(double salary) {
+			return 1000;	
+		}
+		public static double StubCalculateOT_22000(double salary, double hour){
+			return 1500;
+		}
+		public static double StubCalculateTax_22000(double salary, double OT) {
+			return 1645;
+		}
+		public static double CalculateNetSalary_22000(double salary, double extrahour)
+		{	double netsalary = 0.0;
+			double socialinsuranc = StubCalculateSocialInsuranceFee_22000(salary);
+			double OT= StubCalculateOT_22000(salary, extrahour);
+			double tax = StubCalculateTax_22000(salary, OT);
+							
+			netsalary = (salary+OT)-tax-socialinsuranc;
+			
+			System.out.println("Net Salary: " + netsalary);
+		
+			return netsalary;
+		}
+	
+	
+	//CalculateSocialInsuranceFee
+		public static double CalculateSocialInsuranceFee(double salary)
+		{	double fee = 0.0;
+			if (salary >= 20000)
+			{	
+				fee = 1000;			
+			}
+			else if ((salary >= 15000)&&(salary < 20000))
+			{	
+				fee = 800;			
+			}
+			else 
+			{	
+				fee = 750; 
+			}
+			PrintSocialInsuranceFee(fee);
+			return fee;
+		}
+		
+		public static void PrintSocialInsuranceFee(double fee)
+		{	
+			System.out.println("Social Insurance Fee: " + fee);		
+		}
+	
+	
+	//CalculateOT
+		public static double CalculateOT(double salary, double hour)
+		{	double OT = 0.0;
+			int rate = 0;
+			
+			if (salary >= 20000)
+			{	
+				rate = 300;			
+			}
+			else if ((salary >= 15000)&&(salary < 20000))
+			{	
+				rate = 250;			
+			}
+			else 
+			{	
+				rate = 200; 
+			}
+			
+			OT = hour*rate;
+			
+			PrintOT(OT);
+			
+			return OT;
+		}
+		public static void PrintOT(double OT)
+		{	
+			System.out.println("Over Time: " + OT);
+		}
+	
+	
+	//CalculateTax
+		public static double CalculateTax(double salary, double OT)
+		{	double tax = 0.0;
+			double income = salary + OT;
+			double taxRate = 0.0;
+			
+			if (income >= 20000)
+			{	taxRate = 0.07;			
+			}
+			else if ((income >= 15000)&&(income < 20000))
+			{	taxRate = 0.05;			
+			}
+			else 
+			{	taxRate = 0.03; 
+			}
+			tax = income*taxRate;
+			
+			PrintTax(tax);
+			
+			return tax;
+		}
+		public static void PrintTax(double tax)
+		{	System.out.println("Tax: " + tax);		
+		}
 }
--- Integration-Testing/src/Top_down_integration/Top_down_test.java
+++ Integration-Testing/src/Top_down_integration/Top_down_test.java
@@ -2,13 +2,126 @@ package Top_down_integration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Top_down_test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	@BeforeEach
+		public void setUp() throws Exception {
+			List_Stubs List_Stubs = new List_Stubs();
+		}
+	//CalculateNetSalary
+		@Test
+		void CalculateNetSalary_14000() {
+			double CalculateNetSalary_14000 = List_Stubs.CalculateNetSalary_14000(14000, 5);
+			assertEquals(13800, CalculateNetSalary_14000);
+		}
+		@Test
+		void CalculateNetSalary_17000() {
+			double CalculateNetSalary = List_Stubs.CalculateNetSalary_17000(17000, 5);
+			assertEquals(16537.5, CalculateNetSalary);
+		}
+		@Test
+		void CalculateNetSalary_22000() {
+			double CalculateNetSalary = List_Stubs.CalculateNetSalary_22000(22000, 5);
+			assertEquals(20855, CalculateNetSalary);
+		}
+	
+	
+	//CalculateSocialInsuranceFee
+		@Test
+		void CalculateSocialInsuranceFee_14000() {
+			double CalculateSocialInsuranceFee = List_Stubs.CalculateSocialInsuranceFee(14000);
+			assertEquals(750, CalculateSocialInsuranceFee);
+		}
+		@Test
+		void CalculateSocialInsuranceFee_17000() {
+			double CalculateSocialInsuranceFee = List_Stubs.CalculateSocialInsuranceFee(17000);
+			assertEquals(800, CalculateSocialInsuranceFee);
+		}
+		@Test
+		void CalculateSocialInsuranceFee_22000() {
+			double CalculateSocialInsuranceFee = List_Stubs.CalculateSocialInsuranceFee(22000);
+			assertEquals(1000, CalculateSocialInsuranceFee);
+		}
+		
+	//PrintSocialInsuranceFee
+		@Test
+		void PrintSocialInsuranceFee_14000(){
+			
+		}
+		@Test
+		void PrintSocialInsuranceFee_17000(){
+			
+		}
+		@Test
+		void PrintSocialInsuranceFee_22000(){
+			
+		}
+		
+	
+	//CalculateOT
+		@Test
+		void CalculateOT_14000() {
+			double CalculateOT = List_Stubs.CalculateOT(14000, 5);
+			assertEquals(1000, CalculateOT);
+		}
+		@Test
+		void CalculateOT_17000() {
+			double CalculateOT = List_Stubs.CalculateOT(17000, 5);
+			assertEquals(1250, CalculateOT);
+		}
+		@Test
+		void CalculateOT_22000() {
+			double CalculateOT = List_Stubs.CalculateOT(22000, 5);
+			assertEquals(1500, CalculateOT);
+		}
+	
+	//PrintOT
+		@Test
+		void PrintOT_14000(){
+			
+		}
+		@Test
+		void PrintOT_17000(){
+			
+		}
+		@Test
+		void PrintOT_22000(){
+			
+		}
+	
+	
+	//CalculateTax
+		@Test
+		void CalculateTax_14000(){
+			double CalculateTax = List_Stubs.CalculateTax(14000, 5);
+			assertEquals(450, CalculateTax);
+		}
+		@Test
+		void CalculateTax_17000(){
+			double CalculateTax = List_Stubs.CalculateTax(17000, 5);
+			assertEquals(912.5, CalculateTax);
+		}
+		@Test
+		void CalculateTax_22000(){
+			double CalculateTax = List_Stubs.CalculateTax(22000, 5);
+			assertEquals(1645, CalculateTax);
+		}
+	//PrintTax
+		@Test
+		void PrintTax_14000() {
+			
+		}
+		@Test
+		void PrintTax_17000() {
+			
+		}
+		@Test
+		void PrintTax_22000() {
+			
+		}
 
 }
